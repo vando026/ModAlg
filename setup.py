2793ffc0e1891bf25875260fa1fd02273dd4eef1
Alain_Vandormael <avandormael@conviva.com>
2025-10-16 20:41:46 -0700

    Add files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modalg',
+    version='0.1.0',
+    author='', 
+    package_dir={'': 'src'},
+    packages=find_packages(where='src')
+)
